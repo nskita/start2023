40f492a37feca9cc694d63730a4c25959329fb8d
Your Name <your_email@whatever.com>
2023-09-09 14:18:11 +0300

    Initial comit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>first</title>
+	</head>
+	<body>
+		<h1><?php echo "Hello, php!"; ?></h1>
+	</body>
+    
+</html>
---
+++ sum.php
@@ -0,0 +1,7 @@
+<?php
+
+$x =3; 
+ $y =6.5; 
+echo $x - round($y) ;
+echo "<br>"
+echo $x - (int)$y ;
